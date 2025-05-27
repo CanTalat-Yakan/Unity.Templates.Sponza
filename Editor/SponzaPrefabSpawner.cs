cc96950af156d309181ff087c16566563e024408
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-27 14:04:50 +0200

    ⠀⠀⠀⠀⠀

--- Editor/SponzaPrefabSpawner.cs
+++ Editor/SponzaPrefabSpawner.cs
@@ -1,36 +1,13 @@
 #if UNITY_EDITOR
 using UnityEditor;
-using UnityEngine;
 
 namespace UnityEssentials
 {
     public static class SponzaPrefabSpawner
     {
-        [MenuItem("GameObject/3D Object/Sponza", false)]
-        private static void SpawnPrefab(MenuCommand menuCommand)
-        {
-            var guids = AssetDatabase.FindAssets("Sponza t:Prefab");
-            if (guids.Length == 0)
-            {
-                Debug.LogError("Sponza prefab not found in project.");
-                return;
-            }
-
-            var path = AssetDatabase.GUIDToAssetPath(guids[0]);
-            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
-            if (prefab == null)
-            {
-                Debug.LogError($"Failed to load prefab at: {path}");
-                return;
-            }
-
-            GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
-            if (menuCommand.context is GameObject parent)
-                GameObjectUtility.SetParentAndAlign(instance, parent);
-
-            Undo.RegisterCreatedObjectUndo(instance, "Spawn " + instance.name);
-            Selection.activeObject = instance;
-        }
+        [MenuItem("GameObject/Environment/Sponza", false)]
+        private static void SpawnPrefab(MenuCommand menuCommand) =>
+            PrefabSpawnerEditor.Instantiate("UnityEssentials_Environment_Sponza", "Sponza");
     }
 }
 #endif
