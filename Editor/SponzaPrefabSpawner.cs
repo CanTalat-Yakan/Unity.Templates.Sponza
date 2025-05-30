55a8801dc89f3698b01214fc028513448e6354d0
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-30 15:11:11 +0200

    ⠀⠀⠀⠀⠀

--- Editor/SponzaPrefabSpawner.cs
+++ Editor/SponzaPrefabSpawner.cs
@@ -6,14 +6,8 @@ namespace UnityEssentials
     public static class SponzaPrefabSpawner
     {
         [MenuItem("GameObject/Essentials/Environments/Sponza", false, priority = 200)]
-        private static void SpawnPrefab(MenuCommand menuCommand)
-        {
-            var prefab = ResourceLoaderEditor.InstantiatePrefab("UnityEssentials_Prefab_Sponza", "Sponza");
-
-            PrefabUtility.UnpackPrefabInstance(prefab,
-                PrefabUnpackMode.Completely,
-                InteractionMode.AutomatedAction);
-        }
+        private static void SpawnPrefab(MenuCommand menuCommand) =>
+            ResourceLoaderEditor.InstantiatePrefab("UnityEssentials_Prefab_Sponza", "Sponza");
     }
 }
 #endif
